81a12a102cf6817e95fc059edf70b96d785f8c00
Yegabebar <m.barbegayet@gmail.com>
2021-02-09 22:30:42 +0100

    Writing in log files done using a single function, see if need to use separate functions for each data type instead (or not)

--- collector.py
+++ collector.py
@@ -6,8 +6,8 @@ Released under the MIT license
 
 """
 import time
+from datetime import datetime
 
-import mysql.connector
 import psutil
 
 
@@ -33,13 +33,16 @@ def get_net_io_counters():
     """Get the net I/O counters
     """
     return psutil.net_io_counters(
-            pernic=True,
-            nowrap=False,
+        pernic=True,
+        nowrap=False,
     )
 
 
-def store_data(data, file):
-    pass
+def store_data(data, file_name):
+    now = str(datetime.now())
+    file_handle = open(file_name, "a")
+    file_handle.write(now + " " + str(data) + "\n")
+    file_handle.close()
 
 
 # def write_to_db(sqlstatement):
@@ -67,19 +70,28 @@ def main():
     """The main function"""
     while True:
         try:
-            time.sleep(10)
+            time.sleep(5)
+            now = datetime.now()
+            dtf = "%d%m%Y"
             cpu = get_cpu_times()
-            print(cpu)
-            virt_mem = get_virtual_memory()
-            print(virt_mem)
-            disk = get_disk_usage()
-            print(disk)
-            counters = get_net_io_counters()
-            print(counters)
+            store_data(cpu, "./" + now.strftime(dtf) + "-cpu.log")
+            print(datetime.now(), cpu)
 
-            for nic_name in counters:
+            virtual_mem = get_virtual_memory()
+            store_data(virtual_mem, "./" + now.strftime(dtf) + "-virtual_mem.log")
+            print(datetime.now(), virtual_mem)
+
+            disk = get_disk_usage()
+            store_data(disk, "./" + now.strftime(dtf) + "-disk.log")
+            print(datetime.now(), disk)
+
+            net_usage = get_net_io_counters()
+            store_data(net_usage, "./" + now.strftime(dtf) + "-net_usage.log")
+            print(datetime.now(), net_usage)
+
+            for nic_name in net_usage:
                 host = "localhost"
-                bytes_sent = counters[nic_name].bytes_sent
+                bytes_sent = net_usage[nic_name].bytes_sent
                 # sqlstatement = f"INSERT INTO " \
                 #                f"net_io_counter " \
                 #                f"(" \
